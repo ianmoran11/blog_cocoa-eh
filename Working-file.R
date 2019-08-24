d37e6f651893d46b886efecc5deaa7ff27f688b3
Ian Moran <ian.moran11.com>
2019-08-24 13:11:50 +1000

    First commit

---
+++ Working-file.R
@@ -0,0 +1,13 @@
+library(blogdown)
+
+install.packages("devtools")
+
+install.packages("Rcpp")
+devtools::install_github('rstudio/blogdown')
+# If New post doesn't move to home pagem, then copy and past .rmd and .html files into the blog folder.
+
+blogdown::build_dir()
+blogdown::build_site()
+blogdown::serve_site()
+
+blogdown::stop_server()
